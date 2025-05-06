b59e904c51eb46b152394a71fee37df708ea14a1
gabo0820 <gabolg121@gmail.com>
2025-05-06 13:59:37 -0500

    gabo

--- src/main/java/com/LootZone/aplication/service/GeneroService.java
+++ src/main/java/com/LootZone/aplication/service/GeneroService.java
@@ -5,4 +5,9 @@ import com.LootZone.aplication.dto.genero.GeneroRequestDTO;
 import com.LootZone.aplication.dto.genero.GeneroResponseDTO;
 
 import java.util.List;
+public interface GeneroService {
+    List<GeneroResponseDTO> listar();
+    GeneroResponseDTO crear(GeneroRequestDTO requestDTO);
+    GeneroJuegosResponseDTO buscarXID(Long id);
+}
 
