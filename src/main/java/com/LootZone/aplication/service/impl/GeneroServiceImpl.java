b5072c74244691d93c5b4885f043e7f87aeef13f
gabo0820 <gabolg121@gmail.com>
2025-05-06 14:00:32 -0500

    gabo

--- src/main/java/com/LootZone/aplication/service/impl/GeneroServiceImpl.java
+++ src/main/java/com/LootZone/aplication/service/impl/GeneroServiceImpl.java
@@ -15,30 +15,4 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class GeneroServiceImpl implements GeneroService {
-    private final GeneroMapper generoMapper;
-    private final GeneroRepository generoRepository;
-    private final JuegoMapper juegoMapper;
 
-    @Override
-    public List<GeneroResponseDTO> listar() {
-        return generoRepository.findAll().stream().map(generoMapper::toDTO).toList();
-    }
-
-    @Override
-    public GeneroResponseDTO crear(GeneroRequestDTO requestDTO) {
-        Genero genero = generoMapper.toEntity(requestDTO);
-        Genero nuevoGenero = generoRepository.save(genero);
-        return generoMapper.toDTO(nuevoGenero);
-    }
-
-    @Override
-    public GeneroJuegosResponseDTO buscarXID(Long id) {
-        List<JuegoResponseDTO> juegosResponseDTOS = generoRepository.findByGeneroId(id).stream().map(juegoMapper::toDTO).toList();
-        Genero genero = generoRepository.findById(id).orElseThrow(()-> new RuntimeException("id no encontrado"));
-        GeneroJuegosResponseDTO generoJuegosResponseDTO = generoMapper.toGeneroJuegosDto(juegosResponseDTOS, genero);
-        return generoJuegosResponseDTO;
-    }
-}
